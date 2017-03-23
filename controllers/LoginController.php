95b9695749fdbb168d110800a8d1536988d8aeee
gechet <scheoriginal@gmail.com>
2017-03-23 13:59:46 +0200

    auth test

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -22,6 +22,7 @@ class LoginController extends Controller
                 break;
             case 'POST':
                 $request = App::$request->post();
+                App::log($request);
                 $code = false;
                 if (!empty($request['login']) 
                         && !empty($request['password'])
