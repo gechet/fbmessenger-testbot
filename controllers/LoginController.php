f00c0e20e6b28aeffe0a0c27f9b6a1d374dee043
gechet <scheoriginal@gmail.com>
2017-03-23 13:51:43 +0200

    auth test

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -22,7 +22,17 @@ class LoginController extends Controller
                 break;
             case 'POST':
                 $request = App::$request->post();
-                
+                $response = [];
+                if (!empty($request['login']) 
+                        && !empty($request['login'])
+                        && $request['login'] == 'root'
+                        && $request['password'] == '111111'
+                ) {
+                    $response['code'] = uniqid();
+                }
+                $response['redirectUrl'] = App::$request->get('redirect_uri');
+                $response['linkingToken'] = App::$request->get('account_linking_token');
+                header('Location: https://www.facebook.com/messenger_platform/account_linking?' . http_build_query($response));
                 break;
 
             default:
